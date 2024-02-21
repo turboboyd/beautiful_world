34b3d46e8c418aabfb22b1cc39c05900884685c8
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-02-21 18:23:30 +0100

    add

--- js/index.js
+++ js/index.js
@@ -5,15 +5,33 @@ let zSpacing = -1000,
   zVals = [];
 
 window.onscroll = function () {
-  let top = document.documentElement.scrollTop,
-    delta = lastPost - top;
+  let top = document.documentElement.scrollTop;
+  delta = lastPost - top;
   lastPost = top;
+
   frames.forEach((n, i) => {
     zVals.push(i * zSpacing + zSpacing);
     zVals[i] += delta * -5.5;
-    let frame = frames[i];
-    let transform = `translateZ(${zVals[i]}px)`;
-    let opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+    frame = frames[i];
+    transform = `translateZ(${zVals[i]}px)`;
+    opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
     frame.setAttribute("style", `transform: ${transform}; opacity: ${opacity}`);
   });
 };
+
+// window.onscroll = function () {
+//   let top = document.documentElement.scrollTop;
+//   delta = lastPost - top;
+//   lastPost = top;
+
+//   frames.forEach(function (n, i) {
+//     zVals.push(i * zSpacing + zSpacing);
+//     zVals[i] += delta * -5.5;
+//     let frame = frames[i],
+//       transform = `translateZ(${zVals[i]}px)`,
+//       opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+//     frame.setAttribute("style", `transform: ${transform}; opacity: ${opacity}`);
+//   });
+// };
+
+window.scrollTo(0, 1);
