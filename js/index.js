e4c6ef4aa81c26dc9b734ae113a66b0b8ac04849
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-02-22 09:09:58 +0100

    add

--- js/index.js
+++ js/index.js
@@ -19,19 +19,6 @@ window.onscroll = function () {
   });
 };
 
-// window.onscroll = function () {
-//   let top = document.documentElement.scrollTop;
-//   delta = lastPost - top;
-//   lastPost = top;
 
-//   frames.forEach(function (n, i) {
-//     zVals.push(i * zSpacing + zSpacing);
-//     zVals[i] += delta * -5.5;
-//     let frame = frames[i],
-//       transform = `translateZ(${zVals[i]}px)`,
-//       opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
-//     frame.setAttribute("style", `transform: ${transform}; opacity: ${opacity}`);
-//   });
-// };
 
 window.scrollTo(0, 1);
