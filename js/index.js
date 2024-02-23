fa47a1ade7eafe6bdc02143f6f8a2500f83852f2
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-02-23 10:34:08 +0100

    add

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let zSpacing = -1000,
 
 window.onscroll = function () {
   let top = document.documentElement.scrollTop;
-  delta = lastPost - top;
+  let delta = lastPost - top;
   lastPost = top;
 
   frames.forEach((n, i) => {
@@ -19,6 +19,20 @@ window.onscroll = function () {
   });
 };
 
-
-
 window.scrollTo(0, 1);
+
+let soundButton = document.querySelector(".soundbutton"),
+  audio = document.querySelector(".audio");
+
+soundButton.addEventListener("click", (e) => {
+  soundButton.classList.toggle("paused");
+  audio.paused ? audio.play() : audio.pause();
+});
+
+window.onfocus = function () {
+  soundButton.classList.contains("paused") ? audio.pause() : audio.play();
+};
+
+window.onblur = function () {
+  audio.pause();
+};
