18fd28c78ec4afe25f3511042bf65770640c62cf
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2024-02-23 10:49:15 +0100

    add

--- js/index.js
+++ js/index.js
@@ -1,38 +1,43 @@
-let zSpacing = -1000,
-  lastPost = zSpacing / 5,
-  $frames = document.getElementsByClassName("frame"),
-  frames = Array.from($frames),
-  zVals = [];
+window.onload = function () {
+  let zSpacing = -1000,
+    lastPost = zSpacing / 5,
+    $frames = document.getElementsByClassName("frame"),
+    frames = Array.from($frames),
+    zVals = [];
 
-window.onscroll = function () {
-  let top = document.documentElement.scrollTop;
-  let delta = lastPost - top;
-  lastPost = top;
+  window.onscroll = function () {
+    let top = document.documentElement.scrollTop;
+    let delta = lastPost - top;
+    lastPost = top;
 
-  frames.forEach((n, i) => {
-    zVals.push(i * zSpacing + zSpacing);
-    zVals[i] += delta * -5.5;
-    frame = frames[i];
-    transform = `translateZ(${zVals[i]}px)`;
-    opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
-    frame.setAttribute("style", `transform: ${transform}; opacity: ${opacity}`);
+    frames.forEach((n, i) => {
+      zVals.push(i * zSpacing + zSpacing);
+      zVals[i] += delta * -5.5;
+      frame = frames[i];
+      transform = `translateZ(${zVals[i]}px)`;
+      opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+      frame.setAttribute(
+        "style",
+        `transform: ${transform}; opacity: ${opacity}`
+      );
+    });
+  };
+
+  window.scrollTo(0, 1);
+
+  let soundButton = document.querySelector(".soundbutton"),
+    audio = document.querySelector(".audio");
+
+  soundButton.addEventListener("click", (e) => {
+    soundButton.classList.toggle("paused");
+    audio.paused ? audio.play() : audio.pause();
   });
-};
 
-window.scrollTo(0, 1);
+  window.onfocus = function () {
+    soundButton.classList.contains("paused") ? audio.pause() : audio.play();
+  };
 
-let soundButton = document.querySelector(".soundbutton"),
-  audio = document.querySelector(".audio");
-
-soundButton.addEventListener("click", (e) => {
-  soundButton.classList.toggle("paused");
-  audio.paused ? audio.play() : audio.pause();
-});
-
-window.onfocus = function () {
-  soundButton.classList.contains("paused") ? audio.pause() : audio.play();
-};
-
-window.onblur = function () {
-  audio.pause();
+  window.onblur = function () {
+    audio.pause();
+  };
 };
